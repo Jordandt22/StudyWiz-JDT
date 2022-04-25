39084ced9ab65a9c89b13e17dd169f7d1fc0bfd5
Jordan <jordandt123411@gmail.com>
2022-04-24 19:29:26 -0700

    Finished Landing Page.

--- client/src/components/pages/Home/LandingPage.js
+++ client/src/components/pages/Home/LandingPage.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+
+// React Router
+import { NavLink } from "react-router-dom";
+
+// MUI
+import { Container } from "@mui/material";
 
 function LandingPage() {
   return (
-    <div>
-      Landing page
-    </div>
-  )
+    <Container
+      style={{
+        background: `url(${
+          process.env.PUBLIC_URL + "/assets/images/sw_studying.png"
+        })`,
+      }}
+      className="landing-page-container"
+    >
+      <div className="shadow">
+        <h1>Need Help Studying?</h1>
+        <div>
+          <p>
+            With <span>StudyWiz</span>, you'll have access to many different
+            tools to help you on your educational journey. Like flashcards,
+            games, and many other activities.
+          </p>
+          <p>
+            You'll also be able to study with others through community sets and
+            explore all your favorite subjects.
+          </p>
+        </div>
+
+        <NavLink to="/signup">Start Now</NavLink>
+      </div>
+    </Container>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
